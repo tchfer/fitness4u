12a1a0cc49baad1731958dfcf765f35febfb75f5
tchfer <tchfernando@gmail.com>
2021-07-15 00:01:48 -0300

    fix exercise timer display

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
 import { StopTrainingComponent } from './stop-training.component';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-current-training',
@@ -13,19 +14,20 @@ export class CurrentTrainingComponent implements OnInit {
   progress = 0;
   timer: number;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private trainingService: TrainingService) { }
 
   ngOnInit(): void {
-    this.startOrResumetimer();  
+    this.startOrResumetimer();
   }
 
   startOrResumetimer() {
+    const step = this.trainingService.getRunningExercise().duration / 100 * 1000;
     this.timer = setInterval(() => {
-      this.progress += 5;
+      this.progress += 1;
       if (this.progress >= 100) {
         clearInterval(this.timer);
       }
-    }, 1000);
+    }, step);
   }
 
   onStop() {
