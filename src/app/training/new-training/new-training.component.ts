d5b6a5789c61225b18a568cf18913997cbf8c550
tchfer <tchfernando@gmail.com>
2021-07-17 02:25:46 -0300

    fix function to get exercise id

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
@@ -12,16 +14,29 @@ import { Exercise } from '../exercise.model';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>;
 
   constructor(
     private trainingService: TrainingService,
-    private db: AngularFirestore) { }
+    private db: AngularFirestore
+    ) { }
 
   public ngOnInit(): void {
     this.exercises = this.db
       .collection('availableExercises')
-      .valueChanges();
+      .snapshotChanges()
+      .pipe(
+        map(docArray => {
+        return docArray.map(doc => {
+          return {
+            id: doc.payload.doc['id'],
+            name: doc.payload.doc.data()['name'],
+            duration: doc.payload.doc.data()['duration'],
+            calories: doc.payload.doc.data()['calories']
+          };
+        });
+      })
+      );
   }
 
   public onStartTraining(form: NgForm): void {
