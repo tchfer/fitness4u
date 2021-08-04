3844c6a4cfb4a2e905b65b1ddcdd30eab8699a0a
tchfer <tchfernando@gmail.com>
2021-08-04 01:41:00 -0300

    fix production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAIErd1NCoaEFpDgsQDr_JTrO8BMmUKxgs',
+    authDomain: 'fitness4u-dd28f.firebaseapp.com',
+    databaseURL: 'https://fitness4u-dd28f-default-rtdb.firebaseio.com',
+    projectId: 'fitness4u-dd28f',
+    storageBucket: 'fitness4u-dd28f.appspot.com',
+    messagingSenderId: '684916160072',
+    appId: '1:684916160072:web:f0499cfca6a0470fe70a7d',
+    measurementId: 'G-36DBLVE6JH'
+  }
 };
