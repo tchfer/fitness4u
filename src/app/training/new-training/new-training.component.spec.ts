efc782ffbd58b2d507c977f532ce3ff3b423010c
tchfer <tchfernando@gmail.com>
2022-06-22 04:48:51 -0300

    initial unit test

--- src/app/training/new-training/new-training.component.spec.ts
+++ src/app/training/new-training/new-training.component.spec.ts
@@ -1,19 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { TrainingService } from '../training.service';
 
 import { NewTrainingComponent } from './new-training.component';
 
-describe('NewTrainingComponent', () => {
+fdescribe('NewTrainingComponent', () => {
   let component: NewTrainingComponent;
   let fixture: ComponentFixture<NewTrainingComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NewTrainingComponent ]
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NewTrainingComponent ],
+      imports: [
+        FormsModule,
+        StoreModule.forRoot({
+          training: () => ({
+            availableExercises: [],
+            runningExercise: null,
+            finishedExercises: []
+          })
+        })
+      ],
+      providers: [
+        {
+          provide: TrainingService,
+          useValue: jasmine.createSpyObj('TrainingService', ['fetchAvailableExercises', 'startExercise'])
+       }
+    ]
     })
     .compileComponents();
-  });
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(NewTrainingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
